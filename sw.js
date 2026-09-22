fcf4d68ec411b6d7b433407279cc7326be9d0e45
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-22 23:24:57 +0200

    Cache iPhone UK flag icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="english-drive-v6";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const CACHE="english-drive-v7";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
