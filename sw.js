d434036d0d3a9a2be5db050f60b1f892a6bde98b
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-22 23:21:54 +0200

    Refresh cache for speed button v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="english-drive-v5";
+const CACHE="english-drive-v6";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
